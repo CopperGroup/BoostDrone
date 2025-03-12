e5f0258e0b0901efb0db3c117e355abd4f2d0312
tymyevhenmpw <tymyevhen@gmail.com>
2025-03-13 00:05:04 +0100

    Initial commit

---
+++ constants/store.js
@@ -0,0 +1,12 @@
+export const Store = {
+  "name": "BoostDrone",
+  "currency": "UAH",
+  "currency_sign": "₴",
+  "domain": "https://fo-scandinavia.vercel.app",
+  "database": "BoostDrone_2025_03_12",
+  "default_image": {
+    "product_card": 0,
+    "product_page": 0,
+    "checkout_page": 0
+  }
+};
